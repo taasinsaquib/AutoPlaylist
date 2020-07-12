17d46bc043605d8929343957d333c1d4b39e7ed6
Taasin Saquib <taasin.saquib@gmail.com>
2020-07-11 18:27:07 -0700

    Scrape (#3)
    * create playlist and add songs to it
    
    * move spotify client code to new file, add function to return song URIs
    
    * function to return array of song URIs given artist and song names
    
    * create album search function as well
    
    * /playlist is a POST endpoint that returns a link to the created playlist
    
    * function to scrape Reddit post JSON and extract song data

--- helpers.js
+++ helpers.js
@@ -1,10 +1,52 @@
-const spotify    = require('./spotifyClient');
+const spotify = require('./spotifyClient');
+const axios   = require('axios');
 
-/*
+
+async function scrapePost(url){
+
+    var results = [];
+
+    try {
+        var thread = await axios(url);
+    } catch (error) {
+        console.log(error);
+    }
+
+    var post = thread.data[0];  // assume the post is always the first element in this array
+    post = post.data.children[0].data.selftext;
+
+    post = post.split("\n");
+    post = post.filter(function (el) {
+        return el != '';
+    });
+
+    var startLine = post.indexOf("#Singles") + 1;   // start from line after 
+    var endLine = post.indexOf("---")               // singles are always last, so the next section is marked with this
+
+    songs = post.slice(startLine, endLine);
+
+    var regex = /(?<artist>.*)\s-\s(?<song>.*)/m; 
+
+    for (var s of songs){
+        let n = s.replace(/&amp;/gm, "&");  // reddit formats & and ' weirdly
+        n = n.replace(/\\'/m, "\'");
+        
+        let match = regex.exec(n);
+        if(match != null){
+            // console.log(match.groups.artist, ",", match.groups.song);
+            results.push({"artist": match.groups.artist, "song": match.groups.song})
+        }
+        else{
+            console.log("ERROR * Couldn't break up this song line * ", s)  // assumes that artist and song are separated by "-"
+        }
+        
+    }
+
+    // console.dir(results, {'maxArrayLength': null});
+    return results;
+}
+
 music = [{"artist: ""Childish Gambino", "song":"3005"}, ...]
-type  = "songs" or "albums"
-*/
-
 async function getSongURIs(songs){
     // todo - just here for dev, remove later
     spotify.setAccessToken(process.env.ACCESS_TOKEN);
@@ -19,7 +61,7 @@ async function getSongURIs(songs){
             var song = await spotify.searchTracks(searchStr, {limit: 1});
             results.push(song.body.tracks.items[0].uri)     // only return 1 result, so hardcode 0 index here
         } catch (error) {
-            console.log(`ERR: ${error} Couldn't find song with artist: , song: ${s.song}`)
+            console.log(`ERR: ${error} - Couldn't find song with artist: , song: ${s.song}`)
         }
     }
 
@@ -29,9 +71,8 @@ async function getSongURIs(songs){
 // similar to above function, but different input and processing
 // can't add album URIs directly to playlists, so
 // todo - search for all the tracks in an album and add those to the results array (only 100 at a time)
-/*
-albums = ["because thE internet", ...]
-*/
+
+//albums = ["because thE internet", ...]
 async function getAlbumURIs(albums){
     // todo - just here for dev, remove later
     spotify.setAccessToken(process.env.ACCESS_TOKEN);
@@ -56,4 +97,4 @@ async function getAlbumURIs(albums){
     return results;
 }
 
-module.exports = {getSongURIs, getAlbumURIs};
+module.exports = {getSongURIs, getAlbumURIs, scrapePost};
